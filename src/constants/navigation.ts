10b6ac30a2fbcef2ceade4a424739311d427b42f
elwaire <elwairestudio@gmail.com>
2026-04-05 10:32:52 +0700

    update

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -6,6 +6,6 @@ export const SECTIONS_SIDEBAR = [
   // { nameKey: "dashboard", icon: Home, href: "/" },
   { nameKey: "vocabularySet", icon: Book, href: "/vocabulary-set" },
   { nameKey: "activeLearning", icon: Zap, href: "/active-learning" },
-  { nameKey: "exam", icon: Notebook, href: "/exam" },
+  // { nameKey: "exam", icon: Notebook, href: "/exam" },
   { nameKey: "feedback", icon: MessageSquareText, href: "/feedback" },
 ];
